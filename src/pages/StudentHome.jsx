8445c8ee293023f0f0e59f28f5b142c5e35249c8
unknown <romanykhairi9@gmail.com>
2026-06-26 02:49:16 +0300

    view all button update in home page

--- src/pages/StudentHome.jsx
+++ src/pages/StudentHome.jsx
@@ -147,7 +147,7 @@ export const StudentHome = () => {
               </button>
             </div>
 
-            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
               <div>
                 <label className="block mb-1 text-xs font-bold text-slate-500 uppercase ml-2">Min Price</label>
                 <input
